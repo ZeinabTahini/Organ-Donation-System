2bbeec0a6f2696b3fb8e597bae4e20d82b1b8314
ZeinabTahini <161215497+ZeinabTahini@users.noreply.github.com>
2024-04-07 23:30:40 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Organ Donor | Home </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"
+    />
+	  <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="assets/images/fav.png" />
+
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="left">
+    <img class="favicon" src="assets/images/fav.png" alt="Favicon">
+    <h1>Organ Donor</h1>
+</div>
+
+
+        <div class="right">
+            <input type="checkbox" id="check">
+            <label for="check" class="checkBtn">
+                <i class="fa fa-bars"></i>
+            </label>
+            <ul class="list">
+                <li><a href="donor/donor-index.php" class="nav-link">Donor</a></li>
+                <li><a href="patient/patient-index.php" class="nav-link">Patient</a></li>
+                <li><a href="hospital/hospital-index.php" class="nav-link">Hospital</a></li>
+				<li><a href="admin/admin-index.php" class="nav-link">Admin</a></li>
+            </ul>
+        </div>
+    </nav>
+
+
+    <script src="script.js"></script>
+</body>
+
+</html>
